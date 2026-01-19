3037a1f96c2f35175e0b81436b311b70af00b0f9
Rizki Jenie <rizkijenie18@gmail.com>
2026-01-19 01:07:04 +0000

    Menambahkan kode sederhana untuk menentukan suatu bilangan termasuk ganjil atau genap

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("hi")
+#Program Ganjil-Genap
+num = int(input("Input your value!"))
+if num < 0:
+    print("invalid")
+elif num % 2 == 0:
+    print("It is an odd number")
+else:
+    print("It is an even number")
+print("That's it")
